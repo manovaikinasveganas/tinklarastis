e0fe847b43ec7c0c2605c45e4f5068e7961831d0
manovaikinasveganas <arina.medv@gmail.com>
2023-10-17 21:05:46 +0300

    filmai

--- kalendorius/days.js
+++ kalendorius/days.js
@@ -1,19 +1,43 @@
 var days = []
 
 days[1] = {
-        movie: "BA",
+        movie: "Šeimos galva (The Family Man)"
 }
 
 days[2] = {
-        movie: "Vienas namuose. Pirma dalis."
+        movie: "Vienas namuose. Pirma dalis (Home alone)"
 }
 
 days[3] = {
+        movie: "Nuostabios Kalėdos (Falling for Christmas)"
+}
+
+days[4] = {
+        movie: "Ketverios Kalėdos (Four Christmases)"
+}
+
+days[5] = {
+        movie: "Paskutinės Kalėdos (Last Christmas)"
+}
+
+days[6] = {
         movie: "AAAAA"
 }
 
 days[7] = {
-        movie: "AAAAA"
+        movie: "Stebuklas 34-ojoje gatvėje (Miracle on 34th Street)"
+}
+
+days[8] = {
+        movie: "Stebuklas 34-ojoje gatvėje (Miracle on 34th Street)"
+}
+
+days[9] = {
+        movie: "Stebuklas 34-ojoje gatvėje (Miracle on 34th Street)"
+}
+
+days[10] = {
+        movie: "Stebuklas 34-ojoje gatvėje (Miracle on 34th Street)"
 }
 
 days[15] = {
