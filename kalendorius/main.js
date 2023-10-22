99eb57a94b17d32043bea667264df461fa00ede8
Laurynas Lazauskas <8823448+lawrence-laz@users.noreply.github.com>
2023-10-22 22:58:36 +0300

    Atvertimo animacija

--- kalendorius/main.js
+++ kalendorius/main.js
@@ -1,6 +1,9 @@
 function renderDay(dayIndex, day) {
         console.log('renering', day)
-        return `<p id="day-content">${day.movie}` +
-                `<zero-md src="kalendorius/days/${dayIndex}.md"></zero-md>` +
+        // TODO: Add movie section.
+        return `<p id="day-content">${true ? '' : day.movie}` +
+                `<zero-md src="kalendorius/days/${dayIndex}.md">` +
+                `<template><link rel="stylesheet" href="kalendorius/markdown-styles.css" /></template>` +
+                `</zero-md>` +
                 `</p> `;
 }
