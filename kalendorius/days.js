987947d6d7bddfdc812f9b1a700d072d8ae55734
Laurynas Lazauskas <8823448+lawrence-laz@users.noreply.github.com>
2023-10-29 17:07:35 +0200

    Uždaryti langeliai

--- kalendorius/days.js
+++ kalendorius/days.js
@@ -1,46 +1,62 @@
 var days = []
 
 days[1] = {
-        movie: "Šeimos galva (The Family Man)"
+        movie: "Šeimos galva (The Family Man)",
+        open: true,
 }
 
 days[2] = {
-        movie: "Vienas namuose. (Home alone)"
+        movie: "Vienas namuose. (Home alone)",
+        open: true,
 }
 
 days[3] = {
-        movie: "Nuostabios Kalėdos (Falling for Christmas)"
+        movie: "Nuostabios Kalėdos (Falling for Christmas)",
+        open: true,
 }
 
 days[4] = {
-        movie: "Ketverios Kalėdos (Four Christmases)"
+        movie: "Ketverios Kalėdos (Four Christmases)",
+        open: false,
 }
 
 days[5] = {
-        movie: "Paskutinės Kalėdos (Last Christmas)"
+        movie: "Paskutinės Kalėdos (Last Christmas)",
+        open: false,
 }
 
 days[6] = {
-        movie: "AAAAA"
+        movie: "AAAAA",
+        open: false,
 }
 
 days[7] = {
-        movie: "Kalėdų karštinė (Jingle all the Way)"
+        movie: "Kalėdų karštinė (Jingle all the Way)",
+        open: true,
 }
 
 days[8] = {
-        movie: "Stebuklas 34-ojoje gatvėje (Miracle on 34th Street)"
+        movie: "Stebuklas 34-ojoje gatvėje (Miracle on 34th Street)",
+        open: false,
 }
 
 days[9] = {
-        movie: "Vienas namuose 2. Pasiklydęs Niujorke (Home Alone 2: Lost in New York)."
+        movie: "Vienas namuose 2. Pasiklydęs Niujorke (Home Alone 2: Lost in New York).",
+        open: true,
 }
 
 days[10] = {
-        movie: "Paskutinės atostogos (Last Holiday)"
+        movie: "Paskutinės atostogos (Last Holiday)",
+        open: false,
 }
 
 days[15] = {
-        movie: "AAAAA"
+        movie: "AAAAA",
+        open: false,
+}
+
+days[25] = {
+        movie: "",
+        open: false,
 }
 
