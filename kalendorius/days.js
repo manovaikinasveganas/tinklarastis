4f658cf05a3faeeab2d4141bb2ba09e666c35fe7
Laurynas Lazauskas <8823448+lawrence-laz@users.noreply.github.com>
2023-12-24 00:55:18 +0200

    enable day 24

--- kalendorius/days.js
+++ kalendorius/days.js
@@ -1,153 +1,153 @@
 var days = []
 
 days[1] = {
-        movie: "Šeimos Galva",
-        movieLink: "https://www.youtube.com/watch?v=Hzw-p2PfBeU",
-        open: true,
+    movie: "Šeimos Galva",
+    movieLink: "https://www.youtube.com/watch?v=Hzw-p2PfBeU",
+    open: true,
 }
 
 days[2] = {
-        movie: "Vienas Namuose",
-        movieLink: "https://youtu.be/Fmih-zWIjGY?feature=shared&t=2",
-        open: true,
+    movie: "Vienas Namuose",
+    movieLink: "https://youtu.be/Fmih-zWIjGY?feature=shared&t=2",
+    open: true,
 }
 
 days[3] = {
-        movie: "Nuostabios Kalėdos",
-        movieLink: "https://youtu.be/bsNIJd45jYM?feature=shared&t=10",
-        open: true,
+    movie: "Nuostabios Kalėdos",
+    movieLink: "https://youtu.be/bsNIJd45jYM?feature=shared&t=10",
+    open: true,
 }
 
 days[4] = {
-        movie: "Ketverios Kalėdos",
-        movieLink: "https://youtu.be/6veo6Js7HUE?feature=shared&t=6",
-        open: true,
+    movie: "Ketverios Kalėdos",
+    movieLink: "https://youtu.be/6veo6Js7HUE?feature=shared&t=6",
+    open: true,
 }
 
 days[5] = {
-        movie: "Paskutinės Kalėdos",
-        movieLink: "https://www.youtube.com/watch?v=z9CEIcmWmtA",
-        open: true,
+    movie: "Paskutinės Kalėdos",
+    movieLink: "https://www.youtube.com/watch?v=z9CEIcmWmtA",
+    open: true,
 }
 
 days[6] = {
-        movie: "Seni Bambekliai",
-        movieLink: "https://www.youtube.com/watch?v=64MH2i5gr_I",
-        open: true,
+    movie: "Seni Bambekliai",
+    movieLink: "https://www.youtube.com/watch?v=64MH2i5gr_I",
+    open: true,
 }
 
 days[7] = {
-        movie: "Kalėdų Karštinė",
-        movieLink: "https://youtu.be/jhJYMEzQA-Q?feature=shared&t=7",
-        open: true,
+    movie: "Kalėdų Karštinė",
+    movieLink: "https://youtu.be/jhJYMEzQA-Q?feature=shared&t=7",
+    open: true,
 }
 
 days[8] = {
-        movie: "Stebuklas 34-ojoje Gatvėje",
-        movieLink: "https://www.youtube.com/watch?v=leDD6Y-Nyqg",
-        open: true,
+    movie: "Stebuklas 34-ojoje Gatvėje",
+    movieLink: "https://www.youtube.com/watch?v=leDD6Y-Nyqg",
+    open: true,
 }
 
 days[9] = {
-        movie: "Vienas Namuose 2\nPasiklydęs Niujorke",
-        movieLink: "https://youtu.be/5h9VDUNtoto?feature=shared&t=7",
-        open: true,
+    movie: "Vienas Namuose 2\nPasiklydęs Niujorke",
+    movieLink: "https://youtu.be/5h9VDUNtoto?feature=shared&t=7",
+    open: true,
 }
 
 days[10] = {
-        movie: "Paskutinės Atostogos",
-        movieLink: "https://www.youtube.com/watch?v=JO0NSRPcPFs",
-        open: true,
+    movie: "Paskutinės Atostogos",
+    movieLink: "https://www.youtube.com/watch?v=JO0NSRPcPFs",
+    open: true,
 }
 
 days[11] = {
-        movie: "Kalėdų Senelis",
-        movieLink: "https://youtu.be/Bx8FX7etF_8?feature=shared&t=1",
-        open: true,
+    movie: "Kalėdų Senelis",
+    movieLink: "https://youtu.be/Bx8FX7etF_8?feature=shared&t=1",
+    open: true,
 }
 
 days[12] = {
-        movie: "Atostogos",
-        movieLink: "https://www.youtube.com/watch?v=wk9caHO3pW0",
-        open: true,
+    movie: "Atostogos",
+    movieLink: "https://www.youtube.com/watch?v=wk9caHO3pW0",
+    open: true,
 }
 
 days[13] = {
-        movie: "Gremlinai",
-        movieLink: "https://www.youtube.com/watch?v=GEu13j7hOvQ",
-        open: true,
+    movie: "Gremlinai",
+    movieLink: "https://www.youtube.com/watch?v=GEu13j7hOvQ",
+    open: true,
 }
 
 days[14] = {
-        movie: "Elfas",
-        movieLink: "https://youtu.be/14o38xfHlXc?feature=shared&t=6",
-        open: true,
+    movie: "Elfas",
+    movieLink: "https://youtu.be/14o38xfHlXc?feature=shared&t=6",
+    open: true,
 }
 
 days[15] = {
-        movie: "Milijonas Šventinių Lempučių",
-        movieLink: "https://youtu.be/g1A2uWWF1ZU?feature=shared&t=6",
-        open: true,
+    movie: "Milijonas Šventinių Lempučių",
+    movieLink: "https://youtu.be/g1A2uWWF1ZU?feature=shared&t=6",
+    open: true,
 }
 
 days[16] = {
-        movie: "Kol Tu Miegojai",
-        movieLink: "https://www.youtube.com/watch?v=nsJxyUvkB_E",
-        open: true,
+    movie: "Kol Tu Miegojai",
+    movieLink: "https://www.youtube.com/watch?v=nsJxyUvkB_E",
+    open: true,
 }
 
 days[17] = {
-        movie: "Kaip Grinčas Kalėdas vogė",
-        movieLink: "https://www.youtube.com/watch?v=YQV5Pr7pWtM",
-        open: true,
+    movie: "Kaip Grinčas Kalėdas vogė",
+    movieLink: "https://www.youtube.com/watch?v=YQV5Pr7pWtM",
+    open: true,
 }
 
 days[18] = {
-        movie: "Lemtingas atsitiktinumas",
-        movieLink: "https://www.youtube.com/watch?v=ePU2Ux9JIMM",
-        open: true,
+    movie: "Lemtingas atsitiktinumas",
+    movieLink: "https://www.youtube.com/watch?v=ePU2Ux9JIMM",
+    open: true,
 }
 
 days[19] = {
-        movie: "Kalėdos pagal Kranksus",
-        movieLink: "https://www.youtube.com/watch?v=KTzyGuB9V6c",
-        open: true,
+    movie: "Kalėdos pagal Kranksus",
+    movieLink: "https://www.youtube.com/watch?v=KTzyGuB9V6c",
+    open: true,
 }
 
 days[20] = {
-        movie: "Tobulos Kalėdos",
-        movieLink: "https://www.youtube.com/watch?v=-TSX_0rwPNc",
-        open: true,
+    movie: "Tobulos Kalėdos",
+    movieLink: "https://www.youtube.com/watch?v=-TSX_0rwPNc",
+    open: true,
 }
 
 days[21] = {
-        movie: "Tokijo krikštatėviai",
-        movieLink: "https://www.youtube.com/watch?v=ZL_2E-HfIZY",
-        open: true,
+    movie: "Tokijo krikštatėviai",
+    movieLink: "https://www.youtube.com/watch?v=ZL_2E-HfIZY",
+    open: true,
 }
 
 days[22] = {
-        movie: "Košmaras prieš Kalėdas",
-        movieLink: "https://www.youtube.com/watch?v=wr6N_hZyBCk",
-        open: true,
+    movie: "Košmaras prieš Kalėdas",
+    movieLink: "https://www.youtube.com/watch?v=wr6N_hZyBCk",
+    open: true,
 }
 
 
 days[23] = {
-        movie: "Kalėdų giesmė",
-        movieLink: "https://www.youtube.com/watch?v=VZ3lr3urgDU",
-        open: true,
+    movie: "Kalėdų giesmė",
+    movieLink: "https://www.youtube.com/watch?v=VZ3lr3urgDU",
+    open: true,
 }
 
 days[24] = {
-        movie: "Tegyvuoja meilė",
-        movieLink: "https://www.youtube.com/watch?v=H9Z3_ifFheQ",
-        open: false,
+    movie: "Tegyvuoja meilė",
+    movieLink: "https://www.youtube.com/watch?v=H9Z3_ifFheQ",
+    open: true,
 }
 
 days[25] = {
-        movie: "Tas nuostabus gyvenimas",
-        movieLink: "https://www.youtube.com/watch?v=iLR3gZrU2Xo",
-        open: false,
+    movie: "Tas nuostabus gyvenimas",
+    movieLink: "https://www.youtube.com/watch?v=iLR3gZrU2Xo",
+    open: false,
 }
 
