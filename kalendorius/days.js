9aef8d8e7acbb576009925f345d45b11ae2cea95
Laurynas Lazauskas <8823448+lawrence-laz@users.noreply.github.com>
2024-11-16 00:16:19 +0200

    WIP

--- kalendorius/days.js
+++ kalendorius/days.js
@@ -4,150 +4,151 @@ days[1] = {
     movie: "Šeimos Galva",
     movieLink: "https://www.youtube.com/watch?v=Hzw-p2PfBeU",
     open: true,
+    image: "../../pav/citrinu_keksas.jpg",
 }
 
 days[2] = {
     movie: "Vienas Namuose",
     movieLink: "https://youtu.be/Fmih-zWIjGY?feature=shared&t=2",
-    open: true,
+    open: false,
 }
 
 days[3] = {
     movie: "Nuostabios Kalėdos",
     movieLink: "https://youtu.be/bsNIJd45jYM?feature=shared&t=10",
-    open: true,
+    open: false,
 }
 
 days[4] = {
     movie: "Ketverios Kalėdos",
     movieLink: "https://youtu.be/6veo6Js7HUE?feature=shared&t=6",
-    open: true,
+    open: false,
 }
 
 days[5] = {
     movie: "Paskutinės Kalėdos",
     movieLink: "https://www.youtube.com/watch?v=z9CEIcmWmtA",
-    open: true,
+    open: false,
 }
 
 days[6] = {
     movie: "Seni Bambekliai",
     movieLink: "https://www.youtube.com/watch?v=64MH2i5gr_I",
-    open: true,
+    open: false,
 }
 
 days[7] = {
     movie: "Kalėdų Karštinė",
     movieLink: "https://youtu.be/jhJYMEzQA-Q?feature=shared&t=7",
-    open: true,
+    open: false,
 }
 
 days[8] = {
     movie: "Stebuklas 34-ojoje Gatvėje",
     movieLink: "https://www.youtube.com/watch?v=leDD6Y-Nyqg",
-    open: true,
+    open: false,
 }
 
 days[9] = {
     movie: "Vienas Namuose 2\nPasiklydęs Niujorke",
     movieLink: "https://youtu.be/5h9VDUNtoto?feature=shared&t=7",
-    open: true,
+    open: false,
 }
 
 days[10] = {
     movie: "Paskutinės Atostogos",
     movieLink: "https://www.youtube.com/watch?v=JO0NSRPcPFs",
-    open: true,
+    open: false,
 }
 
 days[11] = {
     movie: "Kalėdų Senelis",
     movieLink: "https://youtu.be/Bx8FX7etF_8?feature=shared&t=1",
-    open: true,
+    open: false,
 }
 
 days[12] = {
     movie: "Atostogos",
     movieLink: "https://www.youtube.com/watch?v=wk9caHO3pW0",
-    open: true,
+    open: false,
 }
 
 days[13] = {
     movie: "Gremlinai",
     movieLink: "https://www.youtube.com/watch?v=GEu13j7hOvQ",
-    open: true,
+    open: false,
 }
 
 days[14] = {
     movie: "Elfas",
     movieLink: "https://youtu.be/14o38xfHlXc?feature=shared&t=6",
-    open: true,
+    open: false,
 }
 
 days[15] = {
     movie: "Milijonas Šventinių Lempučių",
     movieLink: "https://youtu.be/g1A2uWWF1ZU?feature=shared&t=6",
-    open: true,
+    open: false,
 }
 
 days[16] = {
     movie: "Kol Tu Miegojai",
     movieLink: "https://www.youtube.com/watch?v=nsJxyUvkB_E",
-    open: true,
+    open: false,
 }
 
 days[17] = {
     movie: "Kaip Grinčas Kalėdas vogė",
     movieLink: "https://www.youtube.com/watch?v=YQV5Pr7pWtM",
-    open: true,
+    open: false,
 }
 
 days[18] = {
     movie: "Lemtingas atsitiktinumas",
     movieLink: "https://www.youtube.com/watch?v=ePU2Ux9JIMM",
-    open: true,
+    open: false,
 }
 
 days[19] = {
     movie: "Kalėdos pagal Kranksus",
     movieLink: "https://www.youtube.com/watch?v=KTzyGuB9V6c",
-    open: true,
+    open: false,
 }
 
 days[20] = {
     movie: "Tobulos Kalėdos",
     movieLink: "https://www.youtube.com/watch?v=-TSX_0rwPNc",
-    open: true,
+    open: false,
 }
 
 days[21] = {
     movie: "Tokijo krikštatėviai",
     movieLink: "https://www.youtube.com/watch?v=ZL_2E-HfIZY",
-    open: true,
+    open: false,
 }
 
 days[22] = {
     movie: "Košmaras prieš Kalėdas",
     movieLink: "https://www.youtube.com/watch?v=wr6N_hZyBCk",
-    open: true,
+    open: false,
 }
 
 
 days[23] = {
     movie: "Kalėdų giesmė",
     movieLink: "https://www.youtube.com/watch?v=VZ3lr3urgDU",
-    open: true,
+    open: false,
 }
 
 days[24] = {
     movie: "Tegyvuoja meilė",
     movieLink: "https://www.youtube.com/watch?v=H9Z3_ifFheQ",
-    open: true,
+    open: false,
 }
 
 days[25] = {
     movie: "Tas nuostabus gyvenimas",
     movieLink: "https://www.youtube.com/watch?v=iLR3gZrU2Xo",
-    open: true,
+    open: false,
 }
 
--- kalendorius/main.js
+++ kalendorius/main.js
@@ -1,35 +1,56 @@
 function renderDay(dayIndex, day) {
-        console.log('renering', day)
-        // TODO: Add movie section.
-        return `<p id="day-content">${true ? '' : day.movie}` +
-                `<zero-md src="kalendorius/days/${dayIndex}.md">` +
-                `<template><link rel="stylesheet" href="kalendorius/markdown-styles.css?v=2" /></template>` +
-                `</zero-md>` +
-                `</p> `;
+    console.log('renering', day)
+    // TODO: Add movie section.
+    return `
+    <div class="row" id="day-content">
+
+        <div class="column-image">
+            <img id="day-image" src="${day.image}" style="width:100%"/>
+        </div>
+
+        <div class="column-text">
+            <p id="day-text-content">${true ? '' : day.movie}
+                <zero-md src="kalendorius/days/${dayIndex}.md">
+                    <template><link rel="stylesheet" href="kalendorius/markdown-styles.css?v=2" /></template>
+                </zero-md>
+
+                <a id="wreath-link" href="" target="_blank" style="align-self: center;">
+                    <div class="wreath">
+                        Šiandienos Kalėdinis filmas<br>
+                        <div id="wreath-title">
+                            Vienas namuose<br>
+                        </div>
+                    </div>
+                </a>
+            </p> 
+        </div>
+        <div class="down-arrow"></div>
+    </div>`
+        ;
 }
 
 function uuidv4() {
-        return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
-                (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-        );
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
 }
 
 function ping(scope) {
 
-        var ref = localStorage.getItem("ref")
-        if (!ref) {
-                ref = uuidv4()
-                localStorage.setItem("ref", ref)
-        }
+    var ref = localStorage.getItem("ref")
+    if (!ref) {
+        ref = uuidv4()
+        localStorage.setItem("ref", ref)
+    }
 
-        const requestOptions = {
-                method: 'PUT',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({
-                        author: '',
-                        reference: 'amlt-' + ref,
-                        content: scope
-                })
-        };
-        fetch('https://vegancity.laz.dev/feedback', requestOptions)
+    const requestOptions = {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            author: '',
+            reference: 'amlt-' + ref,
+            content: scope
+        })
+    };
+    fetch('https://vegancity.laz.dev/feedback', requestOptions)
 }
