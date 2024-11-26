4f285043c8ab644aee907c94f66bf26683933709
Laurynas Lazauskas <8823448+lawrence-laz@users.noreply.github.com>
2024-11-26 22:05:32 +0200

    atnaujinta išvaizda

--- kalendorius/days.js
+++ kalendorius/days.js
@@ -3,151 +3,175 @@ var days = []
 days[1] = {
     movie: "Edvardas Žirkliarankis",
     movieLink: "https://www.youtube.com/watch?v=TBHIO60whNw",
-    open: false,
+    open: true,
+    image: "../../pav/zagareliai.jpg",
 }
 
 days[2] = {
     movie: "Bridžitos Džones dienoraštis",
     movieLink: "https://www.youtube.com/watch?v=xjlKnDWZYzc",
-    open: false,
+    open: true,
+    image: "../../pav/keptos_bulves_2.jpg",
 }
 
 days[3] = {
     movie: "Neklaužados ir geručiai",
     movieLink: "https://www.youtube.com/watch?v=W9VgnoenO4g",
-    open: false,
+    open: true,
+    image: "../../pav/kiras.jpg",
 }
 
 days[4] = {
     movie: "Kietas riešutėlis",
     movieLink: "https://www.youtube.com/watch?v=jaJuwKCmJbY",
-    open: false,
+    open: true,
+    image: "../../pav/keptos_duonos_misraine.jpg",
 }
 
 days[5] = {
     movie: "Krampus",
     movieLink: "https://www.youtube.com/watch?v=h6cVyoMH4QE",
-    open: false,
+    open: true,
+    image: "../../pav/pusryciai.jpg",
 }
 
 days[6] = {
     movie: "Something from Tiffany's",
     movieLink: "https://www.youtube.com/watch?v=G5u8oySXNTk",
-    open: false,
+    open: true,
+    image: "../../pav/bolivine-salotos-2.jpg",
 }
 
 days[7] = {
     movie: "Kvaišų šeimynėlė švenčia Kalėdas. 1989 m.",
     movieLink: "https://www.youtube.com/watch?v=NBTTipJX-h4",
-    open: false,
+    open: true,
+    image: "../../pav/grybukai.jpg",
 }
 
 days[8] = {
     movie: "Apsikeitimas vietomis. 1983 m.",
     movieLink: "https://www.youtube.com/watch?v=Fupg2r1EJ9w",
-    open: false,
+    open: true,
+    image: "../../pav/tofu_anakardziu_padaze.jpg",
 }
 
 days[9] = {
     movie: "Denis. Grėsmė visuomenei. Kalėdos",
     movieLink: "https://www.youtube.com/watch?v=5pq2KIL6Vy4",
-    open: false,
+    open: true,
+    image: "../../pav/gnocchi.jpg",
 }
 
 days[10] = {
     movie: "Berniukas vardu Kalėdos",
     movieLink: "https://www.youtube.com/watch?v=aFI_aiidke0",
-    open: false,
+    open: true,
+    image: "../../pav/karstos_salotos.jpg",
 }
 
 days[11] = {
     movie: "Kalėdos (Noel)",
     movieLink: "https://www.youtube.com/watch?v=8Dcq9P8l3B8",
-    open: false,
+    open: true,
+    image: "../../pav/trupininis_pyragas.jpg",
 }
 
 days[12] = {
     movie: "Tyli naktis",
     movieLink: "https://www.youtube.com/watch?v=MukZcsN1ySU",
-    open: false,
+    open: true,
+    image: "../../pav/ryziu_pudingas.jpg",
 }
 
 days[13] = {
     movie: "Gremlinai",
     movieLink: "https://www.youtube.com/watch?v=GEu13j7hOvQ",
     open: true,
+    image: "../../pav/baklazanu_juosteles.jpg",
 }
 
 days[14] = {
     movie: "Elfas",
     movieLink: "https://youtu.be/14o38xfHlXc?feature=shared&t=6",
     open: true,
+    image: "../../pav/spinduliniu_pupuoliu_paploteliai.jpg",
 }
 
 days[15] = {
     movie: "Milijonas Šventinių Lempučių",
     movieLink: "https://youtu.be/g1A2uWWF1ZU?feature=shared&t=6",
     open: true,
+    image: "../../pav/meduoliai.jpg",
 }
 
 days[16] = {
     movie: "Kol Tu Miegojai",
     movieLink: "https://www.youtube.com/watch?v=nsJxyUvkB_E",
     open: true,
+    image: "../../pav/ziedinis_kopustas.jpg",
 }
 
 days[17] = {
     movie: "Kaip Grinčas Kalėdas vogė",
     movieLink: "https://www.youtube.com/watch?v=YQV5Pr7pWtM",
     open: true,
+    image: "../../pav/vafliai.jpg",
 }
 
 days[18] = {
     movie: "Lemtingas atsitiktinumas",
     movieLink: "https://www.youtube.com/watch?v=ePU2Ux9JIMM",
     open: true,
+    image: "../../pav/karis.jpg",
 }
 
 days[19] = {
     movie: "Kalėdos pagal Kranksus",
     movieLink: "https://www.youtube.com/watch?v=KTzyGuB9V6c",
     open: true,
+    image: "../../pav/karis.jpg",
 }
 
 days[20] = {
     movie: "Tobulos Kalėdos",
     movieLink: "https://www.youtube.com/watch?v=-TSX_0rwPNc",
     open: true,
+    image: "../../pav/medutis.jpg",
 }
 
 days[21] = {
     movie: "Tokijo krikštatėviai",
     movieLink: "https://www.youtube.com/watch?v=ZL_2E-HfIZY",
     open: true,
+    image: "../../pav/tofu.jpg",
 }
 
 days[22] = {
     movie: "Košmaras prieš Kalėdas",
     movieLink: "https://www.youtube.com/watch?v=wr6N_hZyBCk",
     open: true,
+    image: "../../pav/tofu.jpg",
 }
-
 
 days[23] = {
     movie: "Kalėdų giesmė",
     movieLink: "https://www.youtube.com/watch?v=VZ3lr3urgDU",
     open: true,
+    image: "../../pav/misraine.jpg",
 }
 
 days[24] = {
     movie: "Tegyvuoja meilė",
     movieLink: "https://www.youtube.com/watch?v=H9Z3_ifFheQ",
     open: true,
+    image: "../../pav/tofinukai.jpg",
 }
 
 days[25] = {
     movie: "Tas nuostabus gyvenimas",
     movieLink: "https://www.youtube.com/watch?v=iLR3gZrU2Xo",
     open: true,
+    image: "../../pav/sokoladas.jpg",
 }
 
--- kalendorius/main.js
+++ kalendorius/main.js
@@ -1,35 +1,66 @@
 function renderDay(dayIndex, day) {
-        console.log('renering', day)
-        // TODO: Add movie section.
-        return `<p id="day-content">${true ? '' : day.movie}` +
-                `<zero-md src="kalendorius/days/${dayIndex}.md">` +
-                `<template><link rel="stylesheet" href="kalendorius/markdown-styles.css?v=2" /></template>` +
-                `</zero-md>` +
-                `</p> `;
+    console.log('renering', day)
+    return `
+    <div class="row" id="day-content">
+
+        <div class="column-image">
+            <img id="day-image" src="${day.image}" style="width:100%;"/>
+        </div>
+
+        <div class="column-text">
+            <p id="day-text-content">${true ? '' : day.movie}
+                <zero-md src="kalendorius/days/${dayIndex}.md">
+                    <template><link rel="stylesheet" href="kalendorius/markdown-styles.css?v=2" /></template>
+                    <template data-append>
+                        <style>
+                            @media (min-aspect-ratio: 3/4) or (max-height: 835px) {
+                                .mobile-image {
+                                    visibility: hidden;
+                                    height: 0px;
+                                    widht: 0px;
+                                }
+                            }
+                        </style>
+                    </template>
+                </zero-md>
+
+                <a id="wreath-link" href="${day.movieLink}" target="_blank" style="align-self: center;">
+                    <div class="wreath">
+                        Šiandienos Kalėdinis filmas<br>
+                        <div id="wreath-title">
+                            ${day.movie}<br>
+                        </div>
+                    </div>
+                </a>
+            </p> 
+        </div>
+        <div class="down-arrow"></div>
+    </div>`
+        ;
 }
 
 function uuidv4() {
-        return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
-                (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-        );
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
 }
 
 function ping(scope) {
 
-        var ref = localStorage.getItem("ref")
-        if (!ref) {
-                ref = uuidv4()
-                localStorage.setItem("ref", ref)
-        }
+    var ref = localStorage.getItem("ref")
+    if (!ref) {
+        ref = uuidv4()
+        localStorage.setItem("ref", ref)
+    }
 
-        const requestOptions = {
-                method: 'PUT',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({
-                        author: '',
-                        reference: 'amlt-' + ref,
-                        content: scope
-                })
-        };
-        fetch('https://vegancity.laz.dev/feedback', requestOptions)
+    const requestOptions = {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            author: '',
+            reference: 'amlt-' + ref,
+            content: scope
+        })
+    };
+    fetch('https://vegancity.laz.dev/feedback', requestOptions)
 }
