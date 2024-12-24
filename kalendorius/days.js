85cdab704087a936ec65cab2efe9b29c703283de
Laurynas Lazauskas <8823448+lawrence-laz@users.noreply.github.com>
2024-12-25 00:11:07 +0200

    open day 25

--- kalendorius/days.js
+++ kalendorius/days.js
@@ -171,7 +171,7 @@ days[24] = {
 days[25] = {
     movie: "Atstumtieji",
     movieLink: "https://www.youtube.com/watch?v=AhKLpJmHhIg",
-    open: false,
+    open: true,
     image: "../../pav/kakava.jpg",
 }
 
