a64273bc50aefc9d1588969ddbd98af5e011d234
Laurynas Lazauskas <8823448+lawrence-laz@users.noreply.github.com>
2025-11-24 22:30:58 +0200

    update run script

--- run-main.sh
+++ run-main.sh
@@ -1,3 +1,3 @@
-chromium-browser --new-window "http://localhost:4000" && bundle exec jekyll serve --livereload
-# open -na "Google Chrome" --args --new-window "http://localhost:4000" && bundle exec jekyll serve --livereload
+# chromium-browser --new-window "http://localhost:4000" && bundle exec jekyll serve --livereload
+open -na "Google Chrome" --args --new-window "http://localhost:4000" && bundle exec jekyll serve --livereload
 # xdg-open http://localhost:4000
