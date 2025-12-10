85ae20d015fe3eac66e5f78c6a7e09920047b87f
Laurynas Lazauskas <8823448+lawrence-laz@users.noreply.github.com>
2025-12-11 00:33:24 +0200

    open day11

--- kalendorius/days.js
+++ kalendorius/days.js
@@ -73,8 +73,8 @@ days[10] = {
 days[11] = {
     movie: "Kalėdos (Noel)",
     movieLink: "https://www.youtube.com/watch?v=8Dcq9P8l3B8",
-    open: false,
-    image: "../../pav/trupininis_pyragas.jpg",
+    open: true,
+    image: "../../pav/puncas-vaikams.jpg",
 }
 
 days[12] = {
